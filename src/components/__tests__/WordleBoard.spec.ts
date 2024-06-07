7d438b4c2f90e89d66d3442e170cb6bd8bbbfdaa
Travis Williams <tw@ctw.dev>
2024-06-07 15:13:18 -0500

    test: :test_tube: Create in-progress test

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -23,5 +23,9 @@ describe('WordleBoard', () => {
 
     expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
   })
-  test.todo('no end-of-game message appears if the user has not yet made a guess')
+  test('no end-of-game message appears if the user has not yet made a guess', async () => {
+    const wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
+    expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
+    expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
+  })
 })
