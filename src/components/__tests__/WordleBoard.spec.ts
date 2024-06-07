a0738a23c72e64d9f4fd1c5973701ec793e3c018
Travis Williams <tw@ctw.dev>
2024-06-07 15:20:34 -0500

    test: :recycle: Extract test scaffolding

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -3,10 +3,13 @@ import WordleBoard from '../WordleBoard.vue'
 import { DEFEAT_MESSAGE, VICTORY_MESSAGE } from '@/settings'
 
 describe('WordleBoard', () => {
-  const wordOfTheDay = 'TESTS'
-  test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
+  let wordOfTheDay = 'TESTS'
+  let wrapper: ReturnType<typeof mount>
 
+  beforeEach(() => {
+    wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
+  })
+  test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
     const guessInput = wrapper.find('input[type=text]')
     await guessInput.setValue(wordOfTheDay)
     await guessInput.trigger('keydown.enter')
@@ -15,8 +18,6 @@ describe('WordleBoard', () => {
   })
 
   test('a defeat message appears if the user makes a guess that is incorrect', async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
-
     const guessInput = wrapper.find('input[type=text]')
     await guessInput.setValue('WRONG')
     await guessInput.trigger('keydown.enter')
