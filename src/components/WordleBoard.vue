a30e295128e6b10dbd92fc6ae6a0f3a33f73762a
Travis Williams <tw@ctw.dev>
2024-06-06 21:59:47 -0500

    test: :white_check_mark: Render Victory Message

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 defineProps<{
-  msg: string;
-}>();
+  wordOfTheDay: string
+}>()
 </script>
 
 <template>
-  <p>{{ msg }}</p>
+  <input type="text" />
+  <p>You won!</p>
+  <!-- <p>{{ wordOfTheDay }}</p> -->
 </template>
